b6efde688c6d8fe0cbd302a0d48f49f6b15303e3
Gabriel Silva Norberto <sn_gabriel@outlook.com>
2021-12-28 09:17:23 -0300

    finished project

--- script.js
+++ script.js
@@ -9,8 +9,9 @@ let instruncoes = document.querySelector('.d-2');
 let etapaAtual = 0;
 let numero = '';
 let votoBranco = false;
+let votos = []; // Enviar os dados para o servidor
 
-// ******************* ETAPAS *******************
+// ******************* ETAPAS (1 ou 2) *******************
 function comecarEtapa() {
     let etapa = etapas[etapaAtual];
 
@@ -18,6 +19,7 @@ function comecarEtapa() {
     numero = '';
     votoBranco = false;
 
+    //Verifica se está na etapa 0 (vereador) ou 1 (prefeito) e adiciona proporcionalmente o número de caixinhas para dígito, que pode ser 5 ou 2 caixas.
     for (let i = 0; i < etapa.numeros; i++) {
         if (i === 0) {
             digitoHtml += '<div class="num pisca"></div>'
@@ -33,35 +35,6 @@ function comecarEtapa() {
     instruncoes.style.display = 'none'; //infos da parte inferior da tela
     imgCandidato.innerHTML = '';
 }
-
-function atualizarInterface() {
-    let etapa = etapas[etapaAtual];
-
-    //Procura candidato
-    //Retorna um array com o candidato selecionado
-    let candidato = etapa.candidatos.filter(candidato => candidato.numero === numero);
-
-    // Encontrou candidato
-    if (candidato.length > 0) {
-        candidato = candidato[0];
-        tituloCandidato.style.display = 'block';
-        dadosCandidato.innerHTML = `Nome: ${candidato.nome} <br/>Partido: ${candidato.partido}`
-        instruncoes.style.display = 'block';
-
-        let fotosHtml = '';
-        for (let i in candidato.fotos) {
-            fotosHtml += `<div class="img-candidato small"> <img src="./images/${candidato.fotos[i].url}" alt="Imagem do Vice Candidato"> ${candidato.fotos[i].legenda} </div>`
-        }
-
-        imgCandidato.innerHTML = fotosHtml;
-    } else {
-        tituloCandidato.style.display = 'block';
-        instruncoes.style.display = 'block';
-        dadosCandidato.innerHTML = '<div class="aviso--grande pisca">VOTO NULO</div>';
-    }
-
-}
-
 
 // ******************* BOTÕES *******************
 function clicou(n) {
@@ -79,6 +52,40 @@ function clicou(n) {
         }
     }
 }
+
+// Procura o Vereador ou Prefeito de acordo ao número digitado pelo usuário e imprime na tela (se existir)
+function atualizarInterface() {
+    let etapa = etapas[etapaAtual];
+
+    //Procura candidato
+    //Retorna um array com o candidato selecionado
+    let candidato = etapa.candidatos.filter(candidato => candidato.numero === numero);
+
+    if (candidato.length > 0) { // Encontrou um candidato
+        candidato = candidato[0];
+        tituloCandidato.style.display = 'block';
+        dadosCandidato.innerHTML = `Nome: ${candidato.nome} <br/>Partido: ${candidato.partido}`
+        instruncoes.style.display = 'block';
+
+        let fotosHtml = '';
+        for (let i in candidato.fotos) {
+            if(candidato.fotos[i].small){
+                fotosHtml += `<div class="img-candidato small"> <img src="./images/${candidato.fotos[i].url}" alt="Imagem do Vice Candidato"> ${candidato.fotos[i].legenda} </div>`
+            } else{
+                fotosHtml += `<div class="img-candidato"> <img src="./images/${candidato.fotos[i].url}" alt="Imagem do Vice Candidato"> ${candidato.fotos[i].legenda} </div>`
+            }
+            
+        }
+        imgCandidato.innerHTML = fotosHtml;
+
+    } else { // Voto Nulo
+        tituloCandidato.style.display = 'block';
+        instruncoes.style.display = 'block';
+        dadosCandidato.innerHTML = '<div class="aviso--grande pisca">VOTO NULO</div>';
+    }
+
+}
+
 //Para votar em branco os dígitos da urna devem estar vazios
 function branco() {
 
@@ -93,19 +100,31 @@ function branco() {
     dadosCandidato.innerHTML = '<div class="aviso--grande pisca">VOTO EM BRANCO</div>';
 
 }
+
+// **************** BOTÃO CORRIGE ****************
 function corrige() {
     comecarEtapa();
 }
+
+// **************** BOTÃO CONFIRMA ****************
 function confirma() {
     let etapa = etapas[etapaAtual];
     let votoConfirmado = false;
 
     if(votoBranco === true){ // voto em branco
         votoConfirmado = true;
-        console.log("Confirmando como Branco");
+        //Array com informações para enviar ao servidor
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: 'branco'
+        });
     } else if(numero.length === etapa.numeros){ //votou em algum candidato ou nulo
         votoConfirmado = true;
-        console.log("Confirmando como Numero");
+        //Array com informações para enviar ao servidor
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: numero
+        });
     }
 
     if(votoConfirmado){
@@ -114,7 +133,7 @@ function confirma() {
         if(etapas[etapaAtual] !== undefined){
             comecarEtapa();
         } else {
-            dadosCandidato.innerHTML = '<div class="aviso--grande pisca">FIM!</div>';
+            document.querySelector('.tela').innerHTML = '<div class="aviso--gigante pisca">FIM!</div>'
         }
     }
 }
